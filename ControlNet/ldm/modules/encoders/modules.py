1d34baa2c0cb28d9a515aa2cb83b6af3b9106f42
KrishO9 <krish27n@gmail.com>
2025-05-15 00:30:48 +0530

    modules.py

--- ControlNet/ldm/modules/encoders/modules.py
+++ ControlNet/ldm/modules/encoders/modules.py
@@ -221,7 +221,7 @@ class FrozenOpenCLIPEmbedder(AbstractEncoder):
         x = self.model.token_embedding(text)  # [batch_size, n_ctx, d_model]
         x = x + self.model.positional_embedding
         x = x.permute(1, 0, 2)  # NLD -> LND
-        x = self.text_transformer_forward(x, attn_mask=self.model.attn_mask)
+        x = self.text_transformer_forward(x, attn_mask=None)
         x = x.permute(1, 0, 2)  # LND -> NLD
         x = self.model.ln_final(x)
         return x
