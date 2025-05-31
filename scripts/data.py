888028ec7d78e14dc65db31c55f8c86bbe049656
KrishO9 <krish27n@gmail.com>
2025-05-31 22:03:01 +0530

    original

--- scripts/data.py
+++ scripts/data.py
@@ -5,8 +5,8 @@ import os # Import os module
 from torch.utils.data import Dataset as TorchDataset # Use an alias to avoid confusion
 
 class Dataset(TorchDataset): # Inherit from the aliased TorchDataset
-    # def __init__(self, prompt_path, location_embeds_path, image_base_dir="/kaggle/input/geosynth64"):
-    def __init__(self, prompt_path, location_embeds_path, image_base_dir="C:/Users/gpu20/Desktop/test/1"):    
+    def __init__(self, prompt_path, location_embeds_path, image_base_dir="/kaggle/input/georeal"):
+    # def __init__(self, prompt_path, location_embeds_path, image_base_dir="C:/Users/gpu20/Desktop/test/1"):    
     # def __init__(self, prompt_path, location_embeds_path, image_base_dir="/home/btech/2022/krish.kahnani22b/GeoSynth/1"):
         # It's safer to open and close the file properly
         with open(prompt_path, "rt") as f:
