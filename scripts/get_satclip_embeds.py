f182bac0a43df629f6c8fce648b6c5b8f490498d
KrishO9 <krish27n@gmail.com>
2025-05-31 23:37:33 +0530

    satclip

--- scripts/get_satclip_embeds.py
+++ scripts/get_satclip_embeds.py
@@ -1,7 +1,7 @@
 import torch
 import pandas as pd
 import numpy as np
-from load import get_satclip
+from satclip.satclip.load import get_satclip
 
 df = pd.read_csv("input_data_list.csv")
 device = "cpu"
